42247f2962f399302e626f0a4cccffbd2c95755d
josue023 <josuejun7@gmail.com>
2022-09-06 19:46:37 -0500

    cambios en la tarea 2 de modulos python

--- modulos_tarea2/main.py
+++ modulos_tarea2/main.py
@@ -3,18 +3,28 @@
 # En el caso de que sean más de las 7, se mostrará un mensaje y en caso contrario,
 # haréis una operación para calcular el tiempo que queda de trabajo.
 
+import time
 from datetime import datetime
 
 def main():
 
+    anio = time.localtime().tm_year
+    mes = time.localtime().tm_mon
+    dia = time.localtime().tm_mday
+
+    #obtenemos la fecha y hora actual y guardamos en la variable "horaActual"
     horaActual = datetime.now()
-    hora2 = datetime(2022, 9, 6, 19, 00, 0)
+
+    #creamos una variable que contenga la fecha actual y la hora que queremos
+    hora2 = datetime(anio, mes, dia, 19, 0, 0)
+
+    #calculamos la diferencia que hay entre las dos horas
     diferencia = hora2 - horaActual
 
     if horaActual >= hora2:
         print("Es la hora de ir a casa")
     else:
-        print("Faltan: ", diferencia)
+        print("Faltan:", diferencia)
 
 
 if __name__ == "__main__":
